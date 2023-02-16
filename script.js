d20ccb461590d5f2fcd3c4ac3d6e75dee58f8ad3
Julio <jcezarsj2gmail.com>
2023-02-16 00:44:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+const items = [
+                
+    {
+        id: 0,
+        nome: 'gastly',
+        img: 'img.png',
+        quantidade: 0
+
+
+    },
+    {
+        id: 1,
+        nome: 'haunter',
+        img: 'img.png',
+        quantidade: 0
+
+
+    },
+
+    {
+        id: 2,
+        nome: 'gengar',
+        img: 'img.png',
+        quantidade: 0
+
+
+    },
+
+
+
+    ]
+
+    inicializarloja = () => {
+        var containerProdutos = document.getElementById(`produtos`);
+        items.map((val)=>{
+            containerProdutos.innerHTML+=`
+            
+            
+                <div class= "produtos-single"> 
+                <img src="`+val.img+ `" />
+                <p>`+val.nome+`</p>
+                <a href="`+val.id+`">Adicionar ao carrinho<a/>
+                
+                
+                
+                
+                
+                
+                <div/>
+            
+            
+            
+            
+            
+            
+            `; 
+
+        })}
+
+        
+
